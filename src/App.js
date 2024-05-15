c2f9f38a05d65f37abf12fc4493c184d9df5d1e2
haehyunpyo <haehyun228@gamil.com>
2024-05-15 17:16:25 +0900

    ListClickEvent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import TestSignup from "./pages/TestSignup";
+import TestListRef from "./pages/TestListRef";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<TestSignup />} />
+        <Route path="/signup" element={<TestSignup />} />
+        <Route path="/" element={<TestListRef />} />
       </Routes>
     </div>
   );
---
+++ src/pages/TestListRef.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+
+function TestListRef() {
+  const prevList = useRef(null);
+
+  const handleClick = (e) => {
+    const clickedList = e.target;
+    const clickedInput = e.target.firstElementChild;
+
+    if (prevList.current != null) {
+      prevList.current.classList.remove("active");
+    }
+
+    clickedList.classList.add("active");
+    prevList.current = clickedList;
+  };
+  return (
+    <>
+      <h3>Test</h3>
+      <Container>
+        <ListBox>
+          <li onClick={handleClick} ref={prevList}>
+            <input type="text" readOnly defaultValue={"리스트1"}></input>
+          </li>
+          <li onClick={handleClick} ref={prevList}>
+            <input type="text" readOnly defaultValue={"리스트2"}></input>
+          </li>
+          <li onClick={handleClick} ref={prevList}>
+            <input type="text" readOnly defaultValue={"리스트3"}></input>
+          </li>
+          <li onClick={handleClick} ref={prevList}>
+            <input type="text" readOnly defaultValue={"리스트4"}></input>
+          </li>
+        </ListBox>
+      </Container>
+    </>
+  );
+}
+
+export default TestListRef;
+
+const Container = styled.div`
+  width: 300px;
+  height: 500px;
+  border: 1px solid gray;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ListBox = styled.ul`
+  width: inherit;
+  margin: auto;
+  padding: 0;
+  list-style: none;
+
+  & li {
+    width: inherit;
+    height: 50px;
+    padding: 0;
+    margin: 30px auto;
+    border: 1px solid black;
+    text-align: center;
+    cursor: pointer;
+    &.active {
+      background-color: gray;
+
+      & input {
+        background-color: gray;
+        color: white;
+      }
+    }
+  }
+
+  & li input {
+    width: 40%;
+    height: 100%;
+    padding: 0;
+    white-space: nowrap;
+    border: none;
+    text-align: center;
+  }
+`;
